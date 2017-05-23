c7188382cf46a1328a109937560ab1a594a47fb8
keshab06 <kcmandal94@gmail.com>
2017-05-23 15:39:13 +0530

    Setting up project
    Setting up project

---
+++ HRLConnect/Index.js
@@ -0,0 +1,13 @@
+﻿/// <reference path="c:\users\k.chandra.mandal\documents\visual studio 2015\Projects\HRLConnect\HRLConnect\Scripts/angular.js" />
+
+var HRLConnect = angular.module("HRLConnect", ['ngRoute']);
+
+HRLConnect.config(['$routeProvider',
+    function ($routeProvider) {
+        $routeProvider.
+        when('/dashboard', {
+            templateUrl:
+        }
+        )
+    }
+])
