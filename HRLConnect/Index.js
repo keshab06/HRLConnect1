e299a45fb5f6361ac8ad2171d923ea6a5f096150
keshab06 <kcmandal94@gmail.com>
2017-05-23 16:45:45 +0530

    Added Routing

--- HRLConnect/Index.js
+++ HRLConnect/Index.js
@@ -1,13 +1,40 @@
 ﻿/// <reference path="c:\users\k.chandra.mandal\documents\visual studio 2015\Projects\HRLConnect\HRLConnect\Scripts/angular.js" />
 
-var HRLConnect = angular.module("HRLConnect", ['ngRoute']);
+//var HRLConnect = angular.module("HRLConnect", ['ngRoute']);
 
-HRLConnect.config(['$routeProvider',
-    function ($routeProvider) {
-        $routeProvider.
-        when('/dashboard', {
-            templateUrl:
+//HRLConnect.config(['$stateProvider',
+//    function ($stateProvider) {
+//        $stateProvider.
+//        state('duSelect', {
+//            url: '/HR&LConnect/DUSelect',
+//            templateUrl: 'duSelect.html',
+//            controllerAs: 'duSelectCtrl'
+//        })
+//        .state('dashboard', {
+//            url: '/HR&LConnect/Dashboard',
+//            templateUrl: 'Views/Dashboard.html',
+//            controllerAs: 'dashboardCtrl'
+//        });
+//    }
+//])
+
+var HRLConnect = angular.module("HRLConnect", []);
+angular
+    .module('HRLConnect')
+    .config(HRLConnectRoutes);
+
+HRLConnectRoutes.$inject = ['$urlRouterProvider', '$stateProvider', '$locationProvider'];
+
+function HRLConnectRoutes($urlRouterProvider, $stateProvider, $locationProvider) {
+
+    $stateProvider
+    .state('dashboard', {
+        url: '/HR&LConnect/Dashboard',
+        templateUrl: 'Views/Dashboard.html',
+        controllerAs: 'dashboardCtrl',
+        data: {
+            pageTitle: "Admin Events",
+            showPageTitle: true,
         }
-        )
-    }
-])
+    });
+}
---
+++ HRLConnect/duSelect.js
@@ -0,0 +1,1 @@
+﻿/// <reference path="c:\users\k.chandra.mandal\documents\visual studio 2015\Projects\HRLConnect\HRLConnect\Scripts/angular.js" />
